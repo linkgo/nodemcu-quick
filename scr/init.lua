1624208a29d5072d8b6dcbbfacd0f6bb0e818df9
Librae <librae8226@gmail.com>
2015-06-23 11:49:16 +0800

    improve, connect mqtt depend on ip hit

--- scr/init.lua
+++ scr/init.lua
@@ -1,3 +1,19 @@
 uart.setup(0,115200,8,0,1)
---tmr.alarm(1, 3000, 0, function() print("start...") dofile("i_node.lua") end)
---tmr.alarm(1, 3000, 0, function() print("start...") dofile("o_node.lua") end)
+gpio.mode(4, gpio.OUTPUT)
+value = false
+tmr.alarm(1, 50, 1, function()
+  value = not value
+  if wifi.sta.getip()== nil then
+    if value == false then
+      gpio.write(4, gpio.LOW)
+    else
+      gpio.write(4, gpio.HIGH)
+    end
+  else
+    tmr.stop(1)
+    gpio.write(4, gpio.HIGH)
+    print("Config done, IP is "..wifi.sta.getip())
+    --dofile("o_node.lua")
+    --dofile("i_node.lua")
+  end
+end)

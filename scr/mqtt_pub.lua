cd05baf2241f12b20bbc83b03266e1184bd3d889
Librae <librae8226@gmail.com>
2015-07-02 13:02:16 +0800

    refine console log

--- scr/mqtt_pub.lua
+++ scr/mqtt_pub.lua
@@ -1,3 +1,4 @@
-m:publish("/nodemcu/input", adc.read(0), 0, 0, function(conn)
-  print("sent")
+data = adc.read(0)
+m:publish("/nodemcu/input", data, 0, 0, function(conn)
+  print("["..string.format("%8.3f", tmr.now()/1000000).."] sent: "..data)
 end)
